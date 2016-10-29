572058d306b5f77ae2220145f6471624a94c4308
Olivier Falardeau <ofalardeau@mirego.com>
2016-10-28 20:44:57 -0400

    removed assemblers/dtos from weight detector

--- src/main/java/ca/ulaval/glo4003/air/api/weightdetection/WeightDetectionResource.java
+++ src/main/java/ca/ulaval/glo4003/air/api/weightdetection/WeightDetectionResource.java
@@ -2,6 +2,7 @@ package ca.ulaval.glo4003.air.api.weightdetection;
 
 import ca.ulaval.glo4003.air.api.weightdetection.dto.WeightDetectionDto;
 import ca.ulaval.glo4003.air.domain.weightdetection.WeightDetectionService;
+import ca.ulaval.glo4003.air.transfer.weightdetection.WeightDetectionAssembler;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -11,15 +12,18 @@ import javax.ws.rs.core.MediaType;
 @Path("/weightDetection")
 public class WeightDetectionResource {
 
-    private WeightDetectionService weightDetectionService;
+    private final WeightDetectionService weightDetectionService;
+    private final WeightDetectionAssembler weightDetectionAssembler;
 
-    public WeightDetectionResource(WeightDetectionService weightDetectionService) {
+    public WeightDetectionResource(WeightDetectionService weightDetectionService, WeightDetectionAssembler weightDetectionAssembler) {
         this.weightDetectionService = weightDetectionService;
+        this.weightDetectionAssembler = weightDetectionAssembler;
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public WeightDetectionDto detectWeight() {
-        return weightDetectionService.detectWeight();
+        double weight = weightDetectionService.detectWeight();
+        return weightDetectionAssembler.create(weight);
     }
 }
