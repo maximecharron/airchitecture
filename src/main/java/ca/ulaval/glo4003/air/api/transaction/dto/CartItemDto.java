8593e7bd1eb8d0dd1dbc0e14ca7b3661fb6d20bb
maxime.charron <maxime.charron@ticketmaster.com>
2016-11-14 07:59:29 -0500

    Add weight and price to transaction

--- src/main/java/ca/ulaval/glo4003/air/api/transaction/dto/CartItemDto.java
+++ src/main/java/ca/ulaval/glo4003/air/api/transaction/dto/CartItemDto.java
@@ -3,6 +3,8 @@ package ca.ulaval.glo4003.air.api.transaction.dto;
 public class CartItemDto {
 
     public int ticketsQuantity;
+    public double luggageWeight;
+    public double ticketsPrice;
     public String arrivalAirport;
     public String airlineCompany;
     public String departureDate;
